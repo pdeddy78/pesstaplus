0b76b3d4c8a36353a6ba70b73b8f9b5aba54acfd
Deddy Pratama <pdeddy78@gmail.com>
2016-05-05 11:33:49 +0700

    Update init.js

--- init.js
+++ init.js
@@ -11,6 +11,7 @@
 	$('.fixed-action-btn').closeFAB();
 	$('input#input_text, textarea#textarea1').characterCounter();
 	$('.tooltipped').tooltip({delay: 50});
+        $('.modal-trigger').leanModal();
 	
 	var input_selector = 'input[type=text], input[type=password], input[type=email], input[type=url], input[type=tel], input[type=number], input[type=search], textarea';
 	
